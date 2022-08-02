29ddf31a7d97ee6a8c519b26d9fc3c7d0c5b80f1
Tim Luchterhand <tim@luchterhand.org>
2022-08-02 13:19:32 +0200

    init

---
+++ Menu.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace DOS2Randomizer {
+    partial class Menu {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.LoadConfig = new System.Windows.Forms.Button();
+            this.layoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.CreateConfig = new System.Windows.Forms.Button();
+            this.centerLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.layoutPanel.SuspendLayout();
+            this.centerLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LoadConfig
+            // 
+            this.LoadConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadConfig.Location = new System.Drawing.Point(3, 9);
+            this.LoadConfig.Name = "LoadConfig";
+            this.LoadConfig.Size = new System.Drawing.Size(628, 56);
+            this.LoadConfig.TabIndex = 0;
+            this.LoadConfig.Text = "Load Config";
+            this.LoadConfig.UseVisualStyleBackColor = true;
+            // 
+            // layoutPanel
+            // 
+            this.layoutPanel.ColumnCount = 1;
+            this.layoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layoutPanel.Controls.Add(this.CreateConfig, 0, 1);
+            this.layoutPanel.Controls.Add(this.LoadConfig, 0, 0);
+            this.layoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutPanel.Location = new System.Drawing.Point(83, 152);
+            this.layoutPanel.Name = "layoutPanel";
+            this.layoutPanel.RowCount = 2;
+            this.layoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.92744F));
+            this.layoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.07256F));
+            this.layoutPanel.Size = new System.Drawing.Size(634, 143);
+            this.layoutPanel.TabIndex = 1;
+            // 
+            // CreateConfig
+            // 
+            this.CreateConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.CreateConfig.Location = new System.Drawing.Point(3, 82);
+            this.CreateConfig.Name = "CreateConfig";
+            this.CreateConfig.Size = new System.Drawing.Size(628, 52);
+            this.CreateConfig.TabIndex = 1;
+            this.CreateConfig.Text = "Create Config";
+            this.CreateConfig.UseVisualStyleBackColor = true;
+            // 
+            // centerLayout
+            // 
+            this.centerLayout.ColumnCount = 3;
+            this.centerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.centerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.centerLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.centerLayout.Controls.Add(this.layoutPanel, 1, 1);
+            this.centerLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.centerLayout.Location = new System.Drawing.Point(0, 0);
+            this.centerLayout.Name = "centerLayout";
+            this.centerLayout.RowCount = 3;
+            this.centerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.centerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.centerLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.centerLayout.Size = new System.Drawing.Size(800, 450);
+            this.centerLayout.TabIndex = 2;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.centerLayout);
+            this.Name = "Menu";
+            this.Text = "DOS2 Randomizer";
+            this.layoutPanel.ResumeLayout(false);
+            this.centerLayout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadConfig;
+        private System.Windows.Forms.TableLayoutPanel layoutPanel;
+        private System.Windows.Forms.Button CreateConfig;
+        private System.Windows.Forms.TableLayoutPanel centerLayout;
+    }
+}
+
