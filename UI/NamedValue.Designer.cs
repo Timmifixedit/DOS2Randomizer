d1842db4de2ce00ada5ba05f3d29943b9687edc2
Tim Luchterhand <tim@luchterhand.org>
2022-08-02 16:14:06 +0200

    added NamedValue control

---
+++ UI/NamedValue.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace DOS2Randomizer.UI {
+    partial class NamedValue {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.layout = new System.Windows.Forms.TableLayoutPanel();
+            this.name = new System.Windows.Forms.Label();
+            this.value = new System.Windows.Forms.NumericUpDown();
+            this.layout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.value)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layout
+            // 
+            this.layout.ColumnCount = 2;
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.42857F));
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.57143F));
+            this.layout.Controls.Add(this.name, 0, 0);
+            this.layout.Controls.Add(this.value, 1, 0);
+            this.layout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layout.Location = new System.Drawing.Point(0, 0);
+            this.layout.Name = "layout";
+            this.layout.RowCount = 1;
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layout.Size = new System.Drawing.Size(272, 41);
+            this.layout.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(3, 13);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(188, 15);
+            this.name.TabIndex = 0;
+            this.name.Text = "name";
+            this.name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // value
+            // 
+            this.value.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.value.Location = new System.Drawing.Point(197, 9);
+            this.value.Name = "value";
+            this.value.Size = new System.Drawing.Size(72, 23);
+            this.value.TabIndex = 1;
+            // 
+            // NamedValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.layout);
+            this.Name = "NamedValue";
+            this.Size = new System.Drawing.Size(272, 41);
+            this.layout.ResumeLayout(false);
+            this.layout.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.value)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel layout;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.NumericUpDown value;
+    }
+}
---
+++ UI/NamedValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DOS2Randomizer.UI {
+    public partial class NamedValue : UserControl {
+
+        public string Label {
+            get => name.Text;
+            set => name.Text = value;
+        }
+
+        public int Value {
+            get => Convert.ToInt32(value.Value);
+            set => this.value.Value = value;
+        }
+
+        public NamedValue() {
+            InitializeComponent();
+        }
+    }
+}
