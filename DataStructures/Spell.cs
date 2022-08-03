4aebb49bb199fb6193b52efaf9ab41d16c4bec0b
Tim Luchterhand <tim@luchterhand.org>
2022-08-03 14:22:07 +0200

    added minimal implementation for Spell data structure

---
+++ DataStructures/Spell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DOS2Randomizer.DataStructures {
+    public class Spell {
+
+        public Spell(string name, string imagePath) {
+            Name = name;
+            ImagePath = imagePath;
+        }
+        public string Name { get; }
+        public string ImagePath { get; }
+
+        // @TODO
+    }
+}
