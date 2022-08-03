177141b1c89522dd6cb720cbf10f2624f85cd326
Tim Luchterhand <tim@luchterhand.org>
2022-08-03 14:22:38 +0200

    added spell list control

---
+++ UI/SpellList.Designer.cs
@@ -0,0 +1,54 @@
+﻿
+namespace DOS2Randomizer.UI {
+    partial class SpellList {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.layout = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // layout
+            // 
+            this.layout.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.layout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layout.Location = new System.Drawing.Point(0, 0);
+            this.layout.Name = "layout";
+            this.layout.Size = new System.Drawing.Size(405, 208);
+            this.layout.TabIndex = 0;
+            // 
+            // ImageList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.layout);
+            this.Name = "ImageList";
+            this.Size = new System.Drawing.Size(405, 208);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel layout;
+    }
+}
---
+++ UI/SpellList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DOS2Randomizer.UI {
+
+    public delegate void ImageClickEvent(DataStructures.Spell spell);
+    public partial class SpellList : UserControl {
+
+        private DataStructures.Spell[] _spells;
+        public ImageClickEvent OnImageClick;
+
+        public DataStructures.Spell[] Spells {
+            get => _spells;
+            set {
+                _spells = value;
+                if (_spells != null) {
+                    RefreshImages();
+                }
+            } 
+        }
+
+        private void HandleClick(int index) {
+            OnImageClick?.Invoke(_spells[index]);
+        }
+
+        private void RefreshImages() {
+            layout.Controls.Clear();
+            for (int index = 0; index < _spells.Length; ++index) {
+                var picture = new PictureBox {Image = Image.FromFile(_spells[index].ImagePath), SizeMode = PictureBoxSizeMode.AutoSize};
+                var i = index;
+                picture.Click += (sender, args) => {
+                    HandleClick(i);
+                };
+
+                layout.Controls.Add(picture);
+            }
+
+            layout.Refresh();
+        }
+
+        public SpellList() {
+            InitializeComponent();
+        }
+    }
+}
