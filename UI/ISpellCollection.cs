cce0da86a502fa1dda0736d8850c419f671c2708
Tim Luchterhand <tim@luchterhand.org>
2022-08-05 15:46:01 +0200

    added SpellCollection interface and spell search control

---
+++ UI/ISpellCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DOS2Randomizer.DataStructures;
+
+namespace DOS2Randomizer.UI {
+    interface ISpellCollection {
+        public Spell[] SpellCollection { get; set; }
+    }
+}
---
+++ UI/SpellSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DOS2Randomizer.DataStructures;
+
+namespace DOS2Randomizer.UI {
+    class SpellSearch : LabeledString {
+        public ISpellCollection ManagedCollection { get; set; }
+
+        public bool CaseSensitive { get; set; }
+
+        public Spell[] AllSpells {
+            get => _spells;
+            set {
+                _spells = value;
+                if (ManagedCollection != null) {
+                    ManagedCollection.SpellCollection = _spells;
+                }
+            }
+
+        }
+
+        private Spell[] _spells;
+
+        private string ManageCase(string s) {
+            return CaseSensitive ? s : s.ToLower();
+        }
+
+        private void Search(string searchString) {
+            ManagedCollection.SpellCollection = AllSpells
+                ?.Where(spell => ManageCase(spell.Name).Contains(ManageCase(searchString))).ToArray();
+        }
+
+        public SpellSearch() {
+            OnValueChanged = Search;
+        }
+    }
+}
