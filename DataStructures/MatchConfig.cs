7f21c6cfa45547dddd24b1a20623d4a1a4f15781
Tim Luchterhand <tim@luchterhand.org>
2022-08-08 13:00:11 +0200

    added player array to match config

--- DataStructures/MatchConfig.cs
+++ DataStructures/MatchConfig.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 
 namespace DOS2Randomizer.DataStructures {
     public class MatchConfig {
+        public const int MaxNumPlayers = 4;
         public const int MaxLevel = 21;
         public string Name { get; set; }
         public int MaxNumMemSlots { get; set; }
@@ -13,22 +14,24 @@ namespace DOS2Randomizer.DataStructures {
         public int K { get; set; }
         public OnLevelUp[] LevelSpecificEvents;
         public Spell[] Spells { get; set; }
+        public Player[] Players { get; set; }
 
         public MatchConfig() {
             LevelSpecificEvents = Enumerable.Range(1, MaxLevel).Select(i => new OnLevelUp(i, 0, 0, 0)).ToArray();
             Spells = new Spell[0];
-
+            Players = new Player[0];
         }
 
         [JsonConstructor]
         public MatchConfig(string name, int maxNumMemSlots, int n, int k, OnLevelUp[] levelSpecificEvents,
-            Spell[] spells) {
+            Spell[] spells, Player[] players) {
             Name = name;
             MaxNumMemSlots = maxNumMemSlots;
             N = n;
             K = k;
             LevelSpecificEvents = levelSpecificEvents;
             Spells = spells;
+            Players = players;
         }
     }
 }
