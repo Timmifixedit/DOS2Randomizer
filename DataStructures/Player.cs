9973f6ad6cfabbe2d5bc28ebdb26a5ab16d5da07
Tim Luchterhand <tim@luchterhand.org>
2022-08-08 13:20:45 +0200

    added json ctor to player

--- DataStructures/Player.cs
+++ DataStructures/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace DOS2Randomizer.DataStructures {
     public enum Attribute {
@@ -20,13 +21,30 @@ namespace DOS2Randomizer.DataStructures {
             None
         }
 
-        public int Level { get; set; }
+        public Player() {
+            Name = "";
+            Level = 1;
+        }
+
+        [JsonConstructor]
+        public Player(string name, int level, Spell[] knownSpells, Spell[] equippedSpells,
+            SkillType[] possibleSkillTypes, Dictionary<Attribute, int> attributes,
+            Dictionary<Spell.School, int> skillPoints) {
+            Name = name;
+            Level = level;
+            KnownSpells = knownSpells;
+            EquippedSpells = equippedSpells;
+            PossibleSkillTypes = possibleSkillTypes;
+            Attributes = attributes;
+            SkillPoints = skillPoints;
+        }
+
         public string Name { get; set; }
+        public int Level { get; set; }
         public Spell[] KnownSpells { get; set; }
         public Spell[] EquippedSpells { get; set; }
         public SkillType[] PossibleSkillTypes { get; set; }
         public Dictionary<Attribute, int> Attributes;
         public Dictionary<Spell.School, int> SkillPoints;
-
     }
 }
