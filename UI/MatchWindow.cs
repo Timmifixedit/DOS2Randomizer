c3c1e82b5c90dc27a53b2e793a8632947beb81d8
Tim Luchterhand <tim@luchterhand.org>
2022-08-08 12:25:49 +0200

    added MatchWindow

---
+++ UI/MatchWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DOS2Randomizer.UI {
+    public partial class MatchWindow : Form {
+        public MatchWindow() {
+            InitializeComponent();
+        }
+    }
+}
