46274a9114885178c8c99f90ca0db70a7afdfbbb
Tim Luchterhand <tim@luchterhand.org>
2022-08-10 23:11:00 +0200

    changed OnLevelUp to immutable record type

--- DataStructures/OnLevelUp.cs
+++ DataStructures/OnLevelUp.cs
@@ -1,19 +1,5 @@
 ﻿using Newtonsoft.Json;
 
 namespace DOS2Randomizer.DataStructures {
-    public class OnLevelUp {
-
-        [JsonConstructor]
-        public OnLevelUp(int level, int newSpells, int newRerolls, int newShuffles) {
-            Level = level;
-            NewSpells = newSpells;
-            NewRerolls = newRerolls;
-            NewShuffles = newShuffles;
-        }
-
-        public int Level { get; }
-        public int NewSpells { get; set; }
-        public int NewRerolls { get; set; }
-        public int NewShuffles { get; set; }
-    }
+    public record OnLevelUp(int Level, int NewSpells, int NewRerolls, int NewShuffles);
 }
