9d47f762579f1b7933fe42b96849ac19d409ef27
Tim Luchterhand <tim@luchterhand.org>
2022-08-10 22:51:52 +0200

    added Logic directoy and skeletton for spell chooser

---
+++ Logic/SpellChooser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DOS2Randomizer.DataStructures;
+
+namespace DOS2Randomizer.Logic {
+    internal static class RandomExtension {
+        public static IEnumerable<T> ChooseRandom<T>(this IEnumerable<T> source, int num) {
+            var rng = new Random();
+            return source.OrderBy(arg => rng.Next()).Take(num).ToList();
+        }
+    }
+
+    public class SpellChooser {
+        private readonly MatchConfig _matchConfig;
+        private readonly Player _player;
+        private readonly int _numSpellsToGenerate;
+        public SpellChooser(MatchConfig matchConfig, Player player) {
+            _matchConfig = matchConfig;
+            _numSpellsToGenerate = _matchConfig.N;
+            _player = player;
+        }
+
+        public IEnumerable<Spell> GetSpells() {
+            // @TODO this is only a dummy implementation
+            return _matchConfig.Spells.Except(_player.KnownSpells).ChooseRandom(_numSpellsToGenerate);
+        }
+    }
+}
