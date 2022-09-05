f408961043c8257b55a242d475ef9fc7718fcd19
Tim Luchterhand <tim@luchterhand.org>
2022-09-05 18:12:20 +0200

    scary dark mode title bar hax from stackoverflow O_o

--- UI/Menu.cs
+++ UI/Menu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using DOS2Randomizer.DataStructures;
 using DOS2Randomizer.Util;
@@ -30,5 +31,22 @@ namespace DOS2Randomizer.UI {
                 window.Activate();
             }
         }
+
+        #region Title bar hax
+
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19;
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+        // stolen from https://stackoverflow.com/questions/57124243/winforms-dark-title-bar-on-windows-10
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e) {
+            if (DwmSetWindowAttribute(Handle, DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1, new[] { 1 }, 4) != 0) {
+                DwmSetWindowAttribute(Handle, DWMWA_USE_IMMERSIVE_DARK_MODE, new[] { 1 }, 4);
+            }
+        }
+
+        #endregion
     }
 }
